d0100365ef7a839fd1f49c5cbfa1299b252cd9a8
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-03-22 23:22:23 -0300

    segundo comit modifico nombres y agrego archivos js

---
+++ js/piedraPapelOtijera.js
@@ -0,0 +1,6 @@
+/*Piedra-papel-tijera 👊🏻 ✋🏻 ✌🏻 :
+crear un script con toda la logica necesaria que solicite al usuario un movimiento 
+(piedra - papel - tijera) la PC elija aleatoriamente un movimiento y al final mostrar
+ por pantalla quien gano, luego preguntar si queremos jugar otra vez. Este ejercicio forma parte del TP4 
+ (se puede realizar con los conceptos vistos hasta hoy)
+*/
