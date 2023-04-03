3f8634ce4331f36c84ee92ff82ef691e8ceadaa3
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-04-03 02:38:38 -0300

    muestro objeto auto por consola y por pantalla

--- js/primer-ejercicio.js
+++ js/primer-ejercicio.js
@@ -8,25 +8,65 @@ objeto.apadar()
 auto encendido
 El auto se apagó
 */
+//Primero modelo el objeto auto, abstraigo las caracteristicas de la vida real antes de comenzar a crearlo.
+
 let auto = {
-    //PROPIEDADES clave: valor
-   color:'azul',
-   marca: 'renault',
-   modelo:'koleos',
-   anio:'2019',
-   id:'aeio123',
-    //METODOS
-   endender: function(){
-      document.write(`<p>El auto esta encendido.</p>`)
-   },
-
-   apagar: function(){
-      document.write(`<p>El auto esta apagado</p>`)
-   }
-}
+  //PROPIEDADES son caracteristicas clave: valor en valor no hay restricciones puede ser cualquier tipo de datos
+  nombre: "auto",
+  color: "azul",
+  marca: "renault",
+  modelo: "koleos",
+  cantPuertas: "5",
+  anio: "2019",
+  id: "aeio123",
+  //METODOS- Acciones
+  endender: () => {
+    document.write(`<p>El auto esta encendido.</p>`);
+  },
+  frenar: function () {
+    document.write(`<p>El auto se detuvo</p>`);
+  },
+  //arrow fuction funcion mas corta
+  tocarBocina: () => {
+    document.write(`El auto tien su bocina encendida`);
+  },
+  apagar: function () {
+    document.write(`<p>El auto esta apagado</p>`);
+  },
+};
 console.log(auto);
-//mostarr por pantalla
-for(){
+console.log(auto.nombre);
 
+for (let propiedad in auto) {
+  if (typeof auto[propiedad] === "function") {
+    console.log(`${propiedad}: [Método del objeto]`);
+  } else {
+    console.log(`${propiedad}: ${auto[propiedad]}`);
+  }
 }
-document.write(`<p>Encendido: ${auto}</p>`)
+
+document.write(`<p>Nombre:${auto.nombre}</p>`);
+document.write(`<p>Color::${auto.color}</p>`);
+document.write(`<p>Marca:${auto.marca}</p>`);
+document.write(`<p>Modelo:${auto.modelo}</p>`);
+document.write(`<p>Cantidad de puertas:${auto.cantPuertas}</p>`);
+document.write(`<p>Año:${auto.anio}</p>`);
+document.write(`<p>Id:${auto.id}</p>`);
+
+// MUESTRO OBJETO CON METODOS
+auto.endender();
+auto.frenar();
+auto.tocarBocina();
+auto.apagar();
+
+
+
+
+// for (let propiedad in auto) {
+//    if (typeof auto[propiedad] === "function") {
+//      document.write(`${propiedad}: [Método del objeto]<br>`);
+
+//  }
+//mostarr por pantalla
+
+// document.write(`<p>Encendido: ${auto.encender}</p>`);
